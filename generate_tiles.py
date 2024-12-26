f4f99a3cb33e2c45797e9ba89c7de32be63c93e0
vadim <89109988814@mail.ru>
2024-12-26 23:41:26 +0000

    ignore +

--- generate_tiles.py
+++ generate_tiles.py
@@ -5,7 +5,7 @@ import subprocess
 # Параметры для генерации тайлов
 zoom_level = 10
 tile_dir = '/var/lib/mod_tile'
-bbox = '-180,-85,180,85' #
+bbox = '-180,-85,180,85' # 123
 
 # Запуск скрипта generate_tiles.py через subprocess
 subprocess.run(['/home/vadim/PycharmProjects/PythonProject/generate_tiles.py', '--zoom', str(zoom_level), '--tile_dir', tile_dir, '--bbox', bbox])
